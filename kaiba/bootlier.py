c757b1ca386c090e53fc682a3c75539e9d6b85c7
Maria Patterson <mtpatter@uw.edu>
2017-11-21 21:19:28 -0800

    Make bootlier bootstrap df

--- kaiba/bootlier.py
+++ kaiba/bootlier.py
@@ -1,31 +1,47 @@
 import numpy as np
+import pandas as pd
 
 __all__ = ['Bootlier']
 
 
 class Bootlier(object):
-    """Calculates the M-TM for a list of points using bootlier.
+    """Calculates the M-TM for a list of points for bootlier."""
 
-    Parameters
-    ----------
-    npoints : `list`
-        List of N points from which to draw samples.
-    z : `int`
-        Number of bootstraps to draw.
-    b : `int`
-        Number of points in each bootstrapped sample.
-    k : `int`
-        Number of points to trim from each extreme side for the trimmed mean.
-    """
+    def __init__(self):
 
+        pass
 
-    def __init__(self, npoints, z, b, k):
-        self.npoints = npoints
-        self.z = z
-        self.b = b
-        self.k = k
+    def _make_samples(self, npoints, b, z):
+        samples = pd.DataFrame(columns=['sample'])
+        for i in range(z):
+            sample = [np.random.choice(npoints, size=b, replace=True)]
+            samples.loc[i] = sample
+        return samples
 
-    def boot(self):
-        bootstrap = np.random.choice(self.npoints, size=self.b, replace=True)
-        return bootstrap
+    def _calc_means(self, samples):
+        samples['mean'] = samples['sample'].apply(lambda x: np.mean(x))
+        return samples
 
+    def _calc_trimmed_means(self, samples, k):
+        samples['sample'] = samples['sample'].apply(lambda x: np.sort(x))
+        samples['trimmed_mean'] = samples['sample'].apply(lambda x: np.mean(x[k:-k]))
+        samples['m_tm'] = samples['mean'].values - samples['trimmed_mean'].values
+        return samples
+
+    def boot(self, npoints, b, z, k):
+        """
+        Parameters
+        ----------
+        npoints : `list`
+            List of N points from which to draw samples.
+        b : `int`
+            Number of points in each bootstrapped sample.
+        z : `int`
+            Number of bootstraps to draw.
+        k : `int`
+            Number of points to trim from each extreme side for the trimmed mean.
+        """
+        df = self._make_samples(npoints, b, z)
+        df = self._calc_means(df)
+        df = self._calc_trimmed_means(df, k)
+        return df
