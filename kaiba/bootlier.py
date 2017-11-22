d944c2d3bcd88fad66ac31a8949fbb3e27e3a962
Maria Patterson <mtpatter@uw.edu>
2017-11-22 10:27:20 -0800

    Fix some naming conventions

--- kaiba/bootlier.py
+++ kaiba/bootlier.py
@@ -5,16 +5,16 @@ __all__ = ['Bootlier']
 
 
 class Bootlier(object):
-    """Calculates the M-TM for a list of points for bootlier."""
+    """Calculates the MTM for a list of points for bootlier."""
 
     def __init__(self):
 
         pass
 
-    def _make_samples(self, npoints, b, z):
+    def _make_samples(self, npoints, z, b):
         samples = pd.DataFrame(columns=['sample'])
-        for i in range(z):
-            sample = [np.random.choice(npoints, size=b, replace=True)]
+        for i in range(b):
+            sample = [np.random.choice(npoints, size=z, replace=True)]
             samples.loc[i] = sample
         return samples
 
@@ -25,23 +25,23 @@ class Bootlier(object):
     def _calc_trimmed_means(self, samples, k):
         samples['sample'] = samples['sample'].apply(lambda x: np.sort(x))
         samples['trimmed_mean'] = samples['sample'].apply(lambda x: np.mean(x[k:-k]))
-        samples['m_tm'] = samples['mean'].values - samples['trimmed_mean'].values
+        samples['mtm'] = samples['mean'].values - samples['trimmed_mean'].values
         return samples
 
-    def boot(self, npoints, b, z, k):
+    def boot(self, npoints, z, b, k):
         """
         Parameters
         ----------
         npoints : `list`
             List of N points from which to draw samples.
-        b : `int`
-            Number of points in each bootstrapped sample.
         z : `int`
+            Number of points in each bootstrapped sample.
+        b : `int`
             Number of bootstraps to draw.
         k : `int`
             Number of points to trim from each extreme side for the trimmed mean.
         """
-        df = self._make_samples(npoints, b, z)
+        df = self._make_samples(npoints, z, b)
         df = self._calc_means(df)
         df = self._calc_trimmed_means(df, k)
         return df
