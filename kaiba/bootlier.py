cc33b51f44abd621bdd67f9ab08d14e87db60063
Maria Patterson <mtpatter@uw.edu>
2017-11-29 15:41:43 -0800

    Rework while loop and replace peak finder

--- kaiba/bootlier.py
+++ kaiba/bootlier.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy import signal
+import peakutils
 from scipy.stats.kde import gaussian_kde
 
 __all__ = ['Bootlier']
@@ -63,29 +63,28 @@ class Bootlier(object):
         mtmlist = self.samples['mtm']
         mtmrange = max(mtmlist) - min(mtmlist)
         x = np.arange(min(mtmlist), max(mtmlist), mtmrange/100.)
-        widths = mtmrange * np.arange(0.01, 1, 0.05)  # widths between peaks
 
         kde_orig = gaussian_kde(mtmlist, bw_method='silverman')
         self.horig = kde_orig.factor
-        hrange = np.arange(0.01*self.horig, 10*self.horig, 0.01*self.horig)
+        hrange = np.arange(0.1*self.horig, 10*self.horig, 0.05*self.horig)
         self.horig_kde = kde_orig
-        peakind = signal.find_peaks_cwt(-kde_orig(x), widths)
+        peakind = peakutils.indexes(kde_orig(x), thres=0.02/max(kde_orig(x)))
         self.horig_peak = [(x[peak], kde_orig(x)[peak]) for peak in peakind]
         self.numpeaks = len(peakind)
 
-        peaks = 1
-        i = -1
-        while peaks == 1:
+        i = 0
+        peaks = 100
+        while peaks > 1:
             hcrit = hrange[i]
             kde = gaussian_kde(mtmlist, bw_method=hcrit)
-            peakind = signal.find_peaks_cwt(-kde(x), widths)
+            peakind = peakutils.indexes(kde(x), thres=0.02/max(kde(x)))
             peaks = len(peakind)
-            hcrit = hrange[i+1]
             kde.set_bandwidth(hcrit)
-            i -= 1
-        self.hcrit = hcrit
-        kde.set_bandwidth(hrange[i+2])
-        peakind = signal.find_peaks_cwt(-kde(x), widths)
+            i += 1
+            peaks = len(peakind)
+        self.hcrit = hrange[i-1]
+        kde = gaussian_kde(mtmlist, bw_method=hrange[i-1])
+        peakind = peakutils.indexes(kde(x), thres=0.02/max(kde(x)))
         self.hcrit_peak = [(x[peak], kde(x)[peak]) for peak in peakind]
         self.hcrit_kde = kde
         self.hratio = self.horig/hcrit
